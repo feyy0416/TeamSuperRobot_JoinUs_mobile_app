a4506f583795bf9710c2a11c74331492829502e0
Momo Jo <shinesday@gmail.com>
2022-04-16 15:17:46 -0700

    login activity

--- app/src/main/java/edu/neu/madcourse/joinus/LoginActivity.java
+++ app/src/main/java/edu/neu/madcourse/joinus/LoginActivity.java
@@ -3,8 +3,19 @@ package edu.neu.madcourse.joinus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btn_sign_in;
+    EditText email;
+    EditText password;
+    TextView forgotPassword;
+    TextView signup;
+    FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
