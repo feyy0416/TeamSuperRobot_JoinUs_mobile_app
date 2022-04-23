9b38019fc9eff55cb991925af8c265dfb717b666
Momo Jo <shinesday@gmail.com>
2022-04-22 20:08:48 -0700

    update main

--- app/src/main/java/edu/neu/madcourse/joinus/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/joinus/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private String cityName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
