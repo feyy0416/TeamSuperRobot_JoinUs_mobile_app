145c0c35616112427a166ba4b689f8935dea7bbe
Momo Jo <shinesday@gmail.com>
2022-04-24 23:25:17 -0700

    update mainpage

--- app/src/main/java/edu/neu/madcourse/joinus/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/joinus/MainActivity.java
@@ -3,12 +3,23 @@ package edu.neu.madcourse.joinus;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
     private String cityName;
+    private RecyclerView hotEvents;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hotEvents = findViewById(R.id.rcv_hot_events);
+        hotEventsRecycler();
+    }
+
+    private void hotEventsRecycler() {
+        hotEvents.setHasFixedSize(true);
+        hotEvents.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
 }
