792bee39a0feebe8e0fa9d5fc80dde7cb79c9668
001496270_TongZhou <44611330+Dabaiee@users.noreply.github.com>
2022-04-28 23:44:51 -0700

    footer

--- app/src/main/java/edu/neu/madcourse/joinus/DetailsActivity.java
+++ app/src/main/java/edu/neu/madcourse/joinus/DetailsActivity.java
@@ -4,6 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -19,6 +23,7 @@ public class DetailsActivity extends AppCompatActivity {
     ImageView img0;
     ImageView img1;
     ImageView img2;
+    ImageButton btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +33,42 @@ public class DetailsActivity extends AppCompatActivity {
         img0 = findViewById(R.id.details_img0);
         img1 = findViewById(R.id.details_img1);
         img2 = findViewById(R.id.details_img2);
-
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = firebaseDatabase.getReference();
-        DatabaseReference getImage = databaseReference.child("Images");
-
-        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.hasChildren()){
-                    for (DataSnapshot item: snapshot.getChildren()){
-                        String url = item.getValue(String.class);
-                        Picasso.get().load(url).into(img0);
-
-                    }
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(DetailsActivity.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                finish();
             }
         });
+
+//        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//        DatabaseReference databaseReference = firebaseDatabase.getReference();
+//        DatabaseReference getImage = databaseReference.child("Images");
+//
+//        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (snapshot.hasChildren()){
+//                    for (DataSnapshot item: snapshot.getChildren()){
+//                        String url = item.getValue(String.class);
+//                        if (url!= null){
+//                            Log.d("test", "item work");
+//                        }
+//                        String url2 = snapshot.getValue(String.class);
+//                        if (url2!= null){
+//                            Log.d("test", "snap work");
+//                        }
+//                        Picasso.get().load(url).into(img0);
+//
+//                    }
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Toast.makeText(DetailsActivity.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
