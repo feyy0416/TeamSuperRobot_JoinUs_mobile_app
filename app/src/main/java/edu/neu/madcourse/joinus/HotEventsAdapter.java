094119e42979a551e88ed3c992135eb2633ed00b
Momo Jo <shinesday@gmail.com>
2022-04-29 22:30:15 -0700

    main activity click listener

--- app/src/main/java/edu/neu/madcourse/joinus/HotEventsAdapter.java
+++ app/src/main/java/edu/neu/madcourse/joinus/HotEventsAdapter.java
@@ -1,5 +1,7 @@
 package edu.neu.madcourse.joinus;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,9 +9,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+
+import edu.neu.madcourse.joinus.auth.LoginActivity;
 
 public class HotEventsAdapter extends RecyclerView.Adapter<HotEventsAdapter.HotEventsHolder> {
     private List<Event> evenList;
@@ -30,6 +35,17 @@ public class HotEventsAdapter extends RecyclerView.Adapter<HotEventsAdapter.HotE
         holder.hotEventImage.setImageResource(evenList.get(position).getImageId());
         holder.hotEventTitle.setText(evenList.get(position).getTitle());
         holder.hotEventDescription.setText(evenList.get(position).getDescription());
+        Event currentEvent = evenList.get(position);
+
+        holder.hot_event_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = holder.hot_event_card.getContext();
+                Intent intent = new Intent(context, DetailsActivity.class);
+                intent.putExtra("eventId", currentEvent.getEventId());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -37,11 +53,13 @@ public class HotEventsAdapter extends RecyclerView.Adapter<HotEventsAdapter.HotE
         return evenList.size();
     }
 
-    public class HotEventsHolder extends RecyclerView.ViewHolder{
+    public class HotEventsHolder extends RecyclerView.ViewHolder {
+
 
         public ImageView hotEventImage;
         public TextView hotEventTitle;
         public TextView hotEventDescription;
+        public CardView hot_event_card;
 
         public HotEventsHolder(@NonNull View itemView) {
             super(itemView);
@@ -49,6 +67,7 @@ public class HotEventsAdapter extends RecyclerView.Adapter<HotEventsAdapter.HotE
             hotEventImage = itemView.findViewById(R.id.hot_event_image);
             hotEventTitle = itemView.findViewById(R.id.hot_event_title);
             hotEventDescription = itemView.findViewById(R.id.hot_event_description);
+            hot_event_card = itemView.findViewById(R.id.hot_event_card);
         }
     }
 }
