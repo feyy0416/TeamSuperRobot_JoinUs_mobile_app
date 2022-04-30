e3c2e03b15ee9c49a27c1cc5714be7571c9ae529
Zifei Song <song.zif@northeastern.edu>
2022-04-30 04:38:06 -0400

    added recyclerView into AccountActivity

---
+++ app/src/main/java/edu/neu/madcourse/joinus/AccountHolder.java
@@ -0,0 +1,28 @@
+package edu.neu.madcourse.joinus;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AccountHolder extends RecyclerView.ViewHolder{
+
+    public TextView title;
+    public TextView time;
+    public TextView description;
+    public ImageView image;
+    public TextView distance;
+    public CardView event_card;
+
+    public AccountHolder(View eventView){
+        super(eventView);
+        title = eventView.findViewById(R.id.event_title);
+        time = eventView.findViewById(R.id.event_time);
+        description = eventView.findViewById(R.id.map_event_description);
+        image = eventView.findViewById(R.id.event_img);
+        distance = eventView.findViewById(R.id.event_location);
+        event_card = eventView.findViewById(R.id.event_card);
+    }
+}
